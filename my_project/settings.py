4b293955fcd6fd961900c6648e2dd3c14212e55b
Bethan McKenzie <bethanmckenzie@gmail.com>
2024-03-04 03:28:08 +0000

    set debug to false

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4js#xj7d^d6##e00wy6cp_hy45=hl(dk#0mgbn1v#coo((n2#u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-bethanmcken-djangoproje-bfu1vi5e643.ws-eu108.gitpod.io']
 
